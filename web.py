cfd071597b2b35c812bfc7d983813579326720c6
Ravil Nagmetov <r.nagmetov@aix.kz>
2023-01-13 15:51:38 +0600

    add index.html

--- web.py
+++ web.py
@@ -1,9 +1,17 @@
 from flask import Flask
+
+f = open("index.html", "r")
+index = f.read()
+
+
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "<h1 style='color:blue'>Hello DevOps 1!</h1"
+    return index
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0')
+    
+    
+f.close()
